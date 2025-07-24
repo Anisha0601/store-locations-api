1b2522eae2fa58d565f556c578017f67b18f0ad1
Anisha Singh <anshusingh79923@gmail.com>
2025-07-24 18:32:11 +0530

    Initial commit: Store Locations GraphQL API

---
+++ api/graphql.js
@@ -0,0 +1,39 @@
+const { ApolloServer } = require('@apollo/server');
+const { startServerAndCreateLambdaHandler, handlers } = require('@as-integrations/aws-lambda');
+const { gql } = require('graphql-tag');
+const stores = require('../storeData');
+
+const typeDefs = gql`
+  type Store {
+    id: ID!
+    name: String!
+    location: String!
+    address: String!
+    status: String!
+    opening_date: String!
+    image: String!
+    shopify_url: String
+  }
+
+  type Query {
+    stores(status: String): [Store]
+    store(id: ID!): Store
+  }
+`;
+
+const resolvers = {
+  Query: {
+    stores: (_, args) => {
+      if (args.status) {
+        return stores.filter(store => store.status === args.status);
+      }
+      return stores;
+    },
+    store: (_, { id }) => stores.find(store => store.id === id),
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+module.exports = startServerAndCreateLambdaHandler(server, handlers.createAPIGatewayProxyEventV2RequestHandler());
+
---
+++ storeData.js
@@ -0,0 +1,15 @@
+// storeData.js
+module.exports = [
+  {
+    id: "fashion-square",
+    name: "Fashion Square",
+    location: "SCOTTSDALE, AZ",
+    address: "7014 E Camelback Rd, STE #1448",
+    status: "active",
+    opening_date: "2023-05-15",
+    image: "https://cdn.shopify.com/s/files/...",
+    shopify_url: "https://your-store.myshopify.com/pages/fashion-square"
+  },
+  // add the rest of your stores here
+];
+
